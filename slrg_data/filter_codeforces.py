a6da7a2348b066a3c6ee5125c85fdc92c719d78a
strinsberg <stevendkom@gamil.com>
2019-07-08 15:28:07 -0600

    Added file for filtering codeforces script. Added some work to the second draft of the report.

---
+++ slrg_data/filter_codeforces.py
@@ -0,0 +1,8 @@
+"""This will be a script to filter out a certain group of users from
+a codeforces users list. Preferably one that has been gendered fully.
+It might also be worth processing that list first to find out what all
+the first names are that are available and possibly trying a different
+method for obtaining gender labels to see what we can get. The
+processing may end up being a script and it may end up just being something
+I do, and just keep the user data.
+"""
