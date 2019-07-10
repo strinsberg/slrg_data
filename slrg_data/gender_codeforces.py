ba8277cc5b1b233c4e1930fb522e5b6e0a55bd52
strinsberg <stevendkom@gamil.com>
2019-07-10 16:18:03 -0600

    added some of the documentation to gender codeforces script.

--- slrg_data/gender_codeforces.py
+++ slrg_data/gender_codeforces.py
@@ -1,5 +1,5 @@
-"""Filters a list of codeforces user records and adds gender to all
-records it can.
+"""Adds gender to all codeforces users it can and writes their records
+to a new file.
 
 The records that have a gender are written to a new file. The records
 that are not able to be labeled, if the api is down, are written to
@@ -17,9 +17,11 @@ Options
 
 
 """
+# Standard python modules
 import sys
 import getopt
 
+# Local imports
 if __name__ == '__main__':
     import collection
     from help_text import GENDER_CODEFORCES as HELP_TEXT
@@ -27,6 +29,7 @@ else:
     from . import collection
     from .help_text import GENDER_CODEFORCES as HELP_TEXT
 
+# Add the directory with the configuration file to the path
 try:
     sys.path.append(collection.common.SLRG_DIR)
     import config  # nopep8, pylint: disable=import-error
@@ -36,11 +39,22 @@ except ModuleNotFoundError:
     sys.exit()
 
 
+# Script and Main Functions ############################################
+
 def _entry():
+    """Script entry point."""
     _script(sys.argv[1:])
 
 
 def _script(argv):
+    """Processes command line arguments and calls main with their values.
+
+    See module details for more info on command line options.
+
+    Args:
+        argv (list of str): The list of command line options and args
+            not containing the script name.
+    """
     # Declare variables
     users_file = None
     gender_table = None
@@ -84,7 +98,15 @@ def _script(argv):
 
 def main(users_file=None, db_login=None, db_passwd=None,
          gender_table=None, gender_file=None, missing_file=None):
+    """Adds gender to all users in a codeforces user list and writes
+    those with gender to a new file.
 
+    Also writes those that cannot have gender found yet to another file
+    for processing later.
+
+    Args:
+        etc.
+    """
     if users_file is None:
         users_file = input("Codeforces users file: ")
     user_list = collection.common.get_json_data(users_file)
@@ -109,7 +131,15 @@ def main(users_file=None, db_login=None, db_passwd=None,
         database.close()
 
 
+# Helpers ##############################################################
+
 def _add_gender(user_list, database, gender_table, gender_file, miss_file):
+    """Does the actual work of filtering the user list and writing the
+    new lists to files.
+
+    Args:
+        etc.
+    """
     gender = []
     missing = []
 
@@ -131,16 +161,23 @@ def _add_gender(user_list, database, gender_table, gender_file, miss_file):
 
 
 def _has_first_name(user_data):
+    """Returns True if a codeforces user record has a first name."""
     return 'firstName' in user_data and user_data['firstName'].strip() != ''
 
 
 def _has_gender(user_data):
+    """Returns True if a codeforces user record has a gender."""
     return user_data['gender'] == 'male' or user_data['gender'] == 'female'
 
 
 def _missing_gender(user_data):
+    """Returns True if a codeforces user record has a first name, but
+    has not been given a gender.
+    """
     return _has_first_name(user_data) and user_data['gender'] is None
 
+
+# Run ##################################################################
 
 if __name__ == "__main__":
     _entry()
